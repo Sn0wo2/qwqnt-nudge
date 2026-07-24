1fcbf2d844672ba2d364bd1575786939c7a07ac8
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2026-07-24 10:21:57 +0800

    chore(package): update deps & fix types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,8 +1,4 @@
-/// <reference types="vite/client" />
-/// <reference types="@qwqnt/types/renderer" />
-
 export {};
-
 declare global {
   var Logs:
     | (new (moduleName: string) => (...args: unknown[]) => void)
